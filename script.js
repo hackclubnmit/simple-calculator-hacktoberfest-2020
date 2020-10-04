825aa9162e85a8d65371386e68ad5fc65c7e5c32
rudyt7 <gallacticsoccer@gmail.com>
2020-10-04 19:16:58 +0530

    added more instructions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Add Selectors here
+// ******Add Selectors here*****
 
 const operationString = document.querySelector('.calculator__operation');
 const resultString = document.querySelector('.calculator__result');
@@ -9,23 +9,29 @@ const addBtn = document.getElementById('+');
 
 const equalBtn = document.getElementById('=');
 
-// add functions here for addition subtraction multiplication and division
+// ****add functions here for addition subtraction multiplication and division*****
 
 const additionHandler = () => {
+  //
+  //
+  //
   console.log('addition here');
   console.log('take all the numbers from the operation string add them');
   console.log('and append the result in the result string then display');
+  //
+  //
+  //
 };
 
 const displayHandler = () => {
   console.log('find out the operation taking place from operation string');
 
-  // call operation methods eg. additionHandler
+  // ***** call operation methods eg. additionHandler *****
 
   console.log('display the result when someone clicks the equal btn');
 };
 
-// add events here
+// ******add events here *******
 
 // btn1.addEventListener('click');
 //
