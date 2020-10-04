887e13e5079740b4b5ab163ae4cb13ee71a9d93b
rudyt7 <gallacticsoccer@gmail.com>
2020-10-04 19:04:33 +0530

    added script files

---
+++ script.js
@@ -0,0 +1,23 @@
+// Add Selectors here
+
+const operationString = document.querySelector('.calculator__operation');
+const resultString = document.querySelector('.calculator__result');
+
+const btn1 = document.getElementById('1');
+
+const addBtn = document.getElementById('+');
+
+// add events here
+
+btn1.addEventListener('click');
+//
+//
+//
+
+addBtn.addEventListener('click', additionHandler);
+
+// add functions here for addition subtraction multiplication and division
+
+const additionHandler = () => {
+  //
+};
